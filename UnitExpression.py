9ccad2698f298c4c1e259d6b85b267f9c8d86488
ShaanK2408 <shaank2@uic.edu>
2025-02-28 11:35:33 -0600

    Add files via upload

---
+++ UnitExpression.py
@@ -0,0 +1,22 @@
+"""
+Group Members:
+  - 
+  - 
+  - 
+"""
+
+# TODO: implement this function
+def min_length(n):
+  """
+  Args:
+    n: an integer
+
+  Returns:
+    the fewest number of 1's in an expression involving only +,*,1,(, and ) which is equal to n, 
+    or -1 if no such expression is possible.
+  """
+  return -1
+
+if __name__ == "__main__":
+  # this should print out 5
+  print(min_length(6))
